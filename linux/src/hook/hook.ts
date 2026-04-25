f06613acf564740bf6694bc3f1a772067adaa9df
qazplm5602 <domi@domi.kr>
2026-04-25 12:00:59 +0900

    feat: 훅 백업 및 클린업 이벤트 구분

--- linux/src/hook/hook.ts
+++ linux/src/hook/hook.ts
@@ -4,5 +4,6 @@ export abstract class Hook {
     protected log = createLogger(this.constructor.name);
 
     public onStart?(): Promise<void>
-    public onFinish?(): Promise<void>
+    public onComplete?(): Promise<void>    // 백업 완료
+    public onFinish?(): Promise<void>      // 백업 및 정리까지 완료
 }
